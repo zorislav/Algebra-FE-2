cb125aa54b3cacc4fddb138daf634077478983d7
Zori <zorislav.sic1@gmail.com>
2026-03-24 20:58:23 +0100

    Od 24.3.

---
+++ 8_Studenti/Zorislav/3_JS/V4_Tipovi_pod/script.js
@@ -0,0 +1,15 @@
+var x = "Lorem ipsum",
+  y = 2345,
+  z = "2345",
+  q = false,
+  w;
+
+//x += "3" + 1;
+x = x + "3" + 1;
+var baaa = "b" + "a" + +"a" + "a";
+
+console.log(x);
+console.log(typeof x);
+
+console.log(baaa);
+console.log(typeof w);
