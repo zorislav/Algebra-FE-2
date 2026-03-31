04be323a93222ddc3e8fedf0ab1e63e4da556cb3
Andro <angrgec@outlook.com>
2026-03-31 20:54:43 +0200

    Promjene 31.3.

---
+++ 8_Studenti/Andro/3_JS/v8_Osnove-programiranja/p2/script.js
@@ -0,0 +1,63 @@
+var password = "01a23456789";
+
+//1a
+function isUnique(str) {
+  return _.uniq(str).length === str.length;
+}
+
+console.log("Opcija 1a: " + isUnique(password));
+
+//1b
+function isUnique2(str) {
+  return new Set(str).size === str.length;
+}
+
+console.log("Opcija 1b: " + isUnique2(password));
+
+//1c
+function isUnique3(str) {
+  var strChars = "";
+  for (var i of str) {
+    if (strChars.includes(i)) return false;
+    strChars = strChars + 1;
+  }
+  return true;
+}
+
+console.log("Opcija 1c: " + isUnique3(password));
+
+//2a
+
+function isNum1(str) {
+  if (_.toNumber(str)) return true;
+  return false;
+}
+
+console.log("Opcija 2a: " + isNum1(password));
+
+//2b
+
+function isNum2(str) {
+  for (var i = 0; i < str.length; i++) {
+    if (str.charCodeAt(i) < 48 || str.charCodeAt(i) > 57) return false;
+  }
+  return true;
+}
+
+console.log("Opcija 2b: " + isNum2(password));
+
+//3a
+
+function reduceLen1(str) {
+  return _.take(str, 10);
+}
+
+console.log("Opcija 3a: " + reduceLen1(password));
+
+//3b
+
+function reduceLen2(str) {
+  return str.substring(0, 10);
+}
+
+console.log("Opcija 3b: " + reduceLen2(password));
