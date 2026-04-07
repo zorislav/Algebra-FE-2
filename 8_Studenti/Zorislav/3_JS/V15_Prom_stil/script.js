4cd47ad6d7f7cff8a50ddc050587de4ec672b377
Zori <zorislav.sic1@gmail.com>
2026-04-07 20:57:51 +0200

    Od 7.4.

---
+++ 8_Studenti/Zorislav/3_JS/V15_Prom_stil/script.js
@@ -0,0 +1,38 @@
+// umjesto circle buleta na elementima liste prikažu redne brojke
+var liEls = document.querySelectorAll("li");
+
+for (var i = 0; i < liEls.length; i++) {
+  liEls[i].style.listStyleType = "decimal";
+}
+// div sa klasom info treba dobiti klasu koja će ga sakriti (dodajte u stylesheet)
+var infoEl = document.querySelector(".info");
+infoEl.classList.add("hide");
+
+// saznajte margin-left izračunati stil koji je primijenjen na body elementu
+var bodyEl = document.querySelector("body");
+var bodyStyles = window.getComputedStyle(bodyEl, null);
+var marginLeft = bodyStyles.getPropertyValue("margin-left");
+console.log(marginLeft);
+
+var marginNum = parseInt(marginLeft);
+var reverse = false;
+//console.log(marginNum);
+
+// animirajte veličinu margine body elementa od pocetne do 30px i natrag u ponavljajućoj petlji
+
+function frame() {
+  if (reverse) {
+    marginNum--;
+  } else {
+    marginNum++;
+  }
+
+  if (marginNum === 30) reverse = true;
+
+  if (marginNum === 0) reverse = false;
+
+  bodyEl.style.marginLeft = marginNum + "px";
+}
+
+var intId = setInterval(frame, 200);
+//clearInterval(intId);
