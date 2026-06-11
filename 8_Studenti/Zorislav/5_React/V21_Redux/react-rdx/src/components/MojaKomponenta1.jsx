dc680c2e62e16bb8a03224600623a0832ad9ca2b
Zori <zorislav.sic1@gmail.com>
2026-06-11 20:54:29 +0200

    Upd 11.6. 2. dio

---
+++ 8_Studenti/Zorislav/5_React/V21_Redux/react-rdx/src/components/MojaKomponenta1.jsx
@@ -0,0 +1,15 @@
+import { useSelector, useDispatch } from "react-redux";
+import { RESET } from "../store/redux-store";
+
+export default function MojaKomponenta1() {
+  const brojac = useSelector((skl) => skl.counter);
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <h2>Ovo je Moja komponenta 1</h2>
+      <p>I moja komponenta koristi Redux store: {brojac}</p>
+      <button onClick={() => dispatch({ type: RESET })}>Reset</button>
+    </>
+  );
+}
